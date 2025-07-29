1f8f504ea3ad43dcdf347599ac4179c7704d10fb
Ömer Faruk Aydın <leviverleihtflugel@gmail.com>
2025-07-29 05:05:08 +0300

    last

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/portfoliov2/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'src/index.html'
+    }
+  }
 });
